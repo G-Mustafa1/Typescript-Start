5770e8642a235be15ef9febd99b8ed01c32d8403
Ghulam Mustafa <157616503+G-Mustafa1@users.noreply.github.com>
2025-01-15 03:14:45 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+// let firstName: string = "Mustafa";
+// let password: number = 121232332;
+// let isAvailable: boolean = true;
+// // firstNamet = true
+// console.log(firstName)
+// const body1 = document.body;
+// const heading = document.createElement('h1');
+// heading.innerText = 'Hello, World! Welcome to TypeScript.';
+// body.appendChild(heading)
+var meinH = document.querySelector('.mein');
+var heading = document.createElement('h1');
+heading.setAttribute('class', 'heading-1');
+heading.textContent = 'Hello, World! Welcome to TypeScript.';
+console.log(heading);
+meinH.appendChild(heading);
